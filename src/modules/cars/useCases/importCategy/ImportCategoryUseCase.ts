660f5717aaefd119e66eb656d6917f62a7c2e4a2
bruno <brmartins92@gmail.com>
2021-10-06 11:26:58 -0300

    ërror file

--- src/modules/cars/useCases/importCategy/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategy/ImportCategoryUseCase.ts
@@ -1,7 +1,7 @@
 import csvParse from "csv-parse";
 import fs from "fs";
 class ImportCategoryUseCase {
-  execute(file: any): void {
+  execute(file: Express.Multer.File | undefined): void {
     const stream = fs.createReadStream(file.path);
 
     const parseFile = csvParse();
