d588426f1d02e5548ea6730286f09598c1effab5
Bruno Martins <brmartins92@gmail.com>
2021-11-18 22:10:44 -0300

    implementation typeorm

--- src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -1,12 +1,11 @@
-//import { ICategoriesRepository } from "../../repositories/interfaces/ICategoriesRepository";
-import { Category } from "../../model/Category";
+import { Category } from "../../entities/Category";
 import { ICategoriesRepository } from "../../repositories/interfaces/ICategoriesRepository";
 
 class ListCategoryUseCase {
   constructor(private categoriesRepository: ICategoriesRepository) {}
 
-  execute(): Category[] {
-    const categories = this.categoriesRepository.list();
+  async execute(): Promise<Category[]> {
+    const categories = await this.categoriesRepository.list();
     return categories;
   }
 }
