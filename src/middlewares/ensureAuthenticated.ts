293a7c3312cc08969b3406e14d26bb3683dc278c
Bruno Martins <brmartins92@gmail.com>
2022-05-30 18:03:38 -0300

    midleaware token

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,5 +1,9 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 
-export async function ensureAuthenticated(request: Request, response: Response) {
+export async function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
+  const authHeader = request.headers.authorization;
 
+  if (!authHeader) {
+    throw new Error("Token missing")
+  }
 }
