fcf22807b5abd9b9bba578f8af414a14d9cee1fa
Bruno Martins <brmartins92@gmail.com>
2022-06-07 22:42:51 -0300

    feat : auth router specification

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,4 +1,11 @@
 import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+import { UserRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+interface IpayLoad {
+  sub: string;
+  user_id: string;
+}
 
 export async function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
   const authHeader = request.headers.authorization;
@@ -6,4 +13,20 @@ export async function ensureAuthenticated(request: Request, response: Response, 
   if (!authHeader) {
     throw new Error("Token missing")
   }
+
+  try {
+    const [ , token ] = authHeader.split(" ");
+    const {sub: user_id } = verify(token, "202cb962ac59075b964b07152d234b70") as IpayLoad;
+    const userRepository = new UserRepository();
+    const user = await userRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User is not exist!")
+    }
+
+    next();
+  } catch (error) {
+    throw new Error('Invalid token');
+  }
+
 }
--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,18 +1,14 @@
 import { Router } from "express";
 
 import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
-import { SpecificationsRepository } from "../modules/cars/repositories/implementations/SpecificationsRepository";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const specificationsRoutes = Router();
-//const specificationsRepository = new SpecificationsRepository();
 
 const createSpecificationController = new CreateSpecificationController();
 
+specificationsRoutes.use(ensureAuthenticated);
 specificationsRoutes.post("/", createSpecificationController.handle);
 
-specificationsRoutes.get("/", (request, response) => {
-  //const all = specificationsRepository.list();
-  //return response.status(201).json(all);
-});
 
 export { specificationsRoutes };
