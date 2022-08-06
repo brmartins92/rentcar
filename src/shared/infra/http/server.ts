ee3e1fb25baa4da9a15af1eb9320758942c82e10
Bruno Martins <brmartins92@gmail.com>
2022-08-06 17:52:16 -0300

    feat: Created seed admin

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,14 +1,18 @@
-import express,{ NextFunction, Response, Request } from "express";
+import express, { NextFunction, Response, Request } from "express";
 import swaggerUI from "swagger-ui-express";
 import "express-async-errors"
-import "@shared/infra/typeorm";
+//import "@shared/infra/typeorm";
 import "reflect-metadata";
+
+
 import "../../container";
 
 import { AppError } from "../../errors/AppError";
 import { router } from "./routes/index";
 import swaggerFile from "../../../swagger.json";
 
+import createConnection from "@shared/infra/typeorm";
+createConnection();
 
 const app = express();
 
@@ -17,8 +21,8 @@ app.use(express.json());
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use(router);
-app.use( (err: Error, request: Request, response: Response, next: NextFunction) => {
-  if ( err instanceof AppError) {
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
     return response.status(err.statusCode).json({
       message: err.message,
     })
